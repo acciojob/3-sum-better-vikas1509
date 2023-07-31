9a8ed127e117a7c0b73491feccb340465453bc5f
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-07-31 13:45:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	arr.sort();
+		let mindiff =Number.MAX_VALUE;
+	let resultSum=arr[0]+arr[1]+arr[2];
+	for(let i=0;i<arr.length-2;i++;){
+let left =i+1;
+		let right =arr.length-1;
+
+		while(left<right){
+			let sum =arr[i]+arr[left]+arr[right];
+			if(sum==target){
+				return sum;
+			}
+			else if (sum<target){
+				left++;
+			}
+			else{
+				right--;
+			}
+let difftarget=Math.abs(sum-target);
+			if(difftarget<mindiff){
+				resultSum = sum;
+				mindiff =difftarget;
+			}
+			
+		}
+		
+	}
+  return resultSum;
 }
 
 module.exports = threeSum;
